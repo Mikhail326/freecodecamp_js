017c64117cab7613ddee178cf12f5e51e26d67c2
Mikhail326 <misha.stelmakh1995@gmail.com>
2022-09-02 19:48:41 +0300

    lessons up to 25

--- main.js
+++ main.js
@@ -3,5 +3,123 @@
 /* My new commit */
 // My new commit //
 
-// lesson 2
+// lesson 2 //    
 var myName;
+
+// lesson 3 //
+var a;
+a = 7
+
+// lesson 4 //
+var a;
+a = 7;
+var b;
+b = a;
+
+// lesson 5 //
+var a = 9;
+
+// lesson 6 //
+var myFirstName = 'Mikhail';
+var myLastName = 'Stelmakh';
+
+// lesson 7 //
+var a = 5;
+var b = 10;
+var c = 'I am a';
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// lesson 8 //
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// lesson 9 //
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// lesson 10 //
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+// lesson 11 //
+const sum = 10 + 10;
+
+// lesson 12 //
+const difference = 45 - 33;
+
+// lesson 13 //
+const product = 8 * 10;
+
+// lesson 14 //
+const quotient = 66 / 33;
+
+// lesson 15 //
+let myVar = 87;
+myVar++
+
+// lesson 16 //
+let myNewVar = 11;
+myNewVar--
+
+// lesson 17 //
+const ourDecimal = 5.7;
+// Only change code below this line
+const myDecimal = 6.2
+
+// lesson 18 //
+const productNew = 2.0 * 2.5;
+
+// lesson 19 //
+const quotientNew = 4.4 / 2.0; // Change this line
+
+// lesson 20 //
+const remainder = 11 % 3;
+
+// lesson 21 //
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+// lesson 22 //
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+// lesson 23 //
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+// lesson 24 //
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /=12;
+b /= 4;
+c /= 11;
+
+// lesson 25 //
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
