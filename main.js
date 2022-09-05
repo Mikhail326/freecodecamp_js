6e23a97ac73910e1c6c2ff94a2075274d3606fa0
Mikhail326 <misha.stelmakh1995@gmail.com>
2022-09-05 08:25:30 +0300

    lessons up to 101

--- main.js
+++ main.js
@@ -735,20 +735,143 @@ function checkObj(obj, checkProp) {
 // lesson 91 //
 const myMusic = [
     {
-      "artist": "Billy Joel",
-      "title": "Piano Man",
-      "release_year": 1973,
-      "formats": [
-        "CD",
-        "8T",
-        "LP"
-      ],
-      "gold": true
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
     },
     {
-      artist: 'Mikhail Stelmakh',
-      title: 'New Albom',
-      release_year: 2022,
-      formats: ['MP3', 'WAVE']
+        artist: 'Mikhail Stelmakh',
+        title: 'New Albom',
+        release_year: 2022,
+        formats: ['MP3', 'WAVE']
     }
-  ];
+];
+
+// lesson 92 //
+const myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+// lesson 93 //
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+const secondTree = myPlants[1].list[1];
+
+// lesson 94 //
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+function updateRecords(records, id, prop, value) {
+    if (prop !== 'tracks' && value !== '') {
+        records[id][prop] = value
+    } else if (prop === 'tracks' && !records[id].hasOwnProperty('tracks')) {
+        records[id][prop] = [value]
+    } else if (prop === 'tracks' && value.length > 0) {
+        records[id][prop].push(value)
+    } else if (value === '') {
+        delete records[id][prop]
+    }
+    return records;
+}
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+// lesson 95 //
+const myArray = [];
+let i = 5
+while (i >= 0) {
+    myArray.push(i)
+    i--
+}
+
+// lesson 96 //
+const myArray = [];
+for (let i = 1; i <= 5; i++) {
+    myArray.push(i)
+}
+
+// lesson 97 //
+const myArray = [];
+for (let i = 1; i < 10; i += 2) {
+    myArray.push(i)
+}
+
+// lesson 98 //
+const myArray = [];
+for (let i = 9; i > 0; i -= 2) {
+    myArray.push(i)
+}
+
+// lesson 99 //
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+    total += myArr[i]
+}
+
+// lesson 100 //
+function multiplyAll(arr) {
+    let product = 1;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            product *= arr[i][j]
+        }
+    }
+    return product;
+}
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+// lesson 101 //
+const myArray = [];
+let i = 10;
+do {
+    myArray.push(i);
+    i++;
+}
+while (i < 11)
