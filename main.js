48dcbee3371f50eb675f9d50d8491c6e48f7223c
Mikhail326 <misha.stelmakh1995@gmail.com>
2022-09-08 11:47:29 +0300

    Completed basic course JS

--- main.js
+++ main.js
@@ -127,6 +127,8 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 // lesson 26 //
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
+// lesson 27 //
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 
 // lesson 28 //
 const myStr = "This is the start." + " " + "This is the end.";
@@ -875,3 +877,127 @@ do {
     i++;
 }
 while (i < 11)
+
+// lesson 102 //
+function sum(arr, n) {
+    if (n <= 0) {
+        return 0
+    } else {
+        return sum(arr, n - 1) + arr[n - 1]
+    }
+}
+
+
+// lesson 103 //
+const contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            if (contacts[i].hasOwnProperty(prop)) {
+                return contacts[i][prop]
+            } else {
+                return 'No such property'
+            }
+        }
+    }
+    return 'No such contact'
+}
+lookUpProfile("Akira", "likes");
+
+// lesson 104 //
+function randomFraction() {
+    return Math.random();
+}
+
+// lesson 105 //
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+}
+
+// lesson 106 //
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
+
+// lesson 107 //
+function convertToInteger(str) {
+    return parseInt(str);
+}
+convertToInteger("56");
+
+// lesson 108 //
+function convertToInteger(str) {
+    return parseInt(str, 2)
+}
+convertToInteger("10011");
+
+// lesson 109 //
+function checkEqual(a, b) {
+    return a === b ? 'Equal' : 'Not Equal'
+}
+checkEqual(1, 2);
+
+// lesson 110 //
+function checkSign(num) {
+    return num > 0 ? 'positive' : num < 0 ? 'negative' : 'zero'
+}
+checkSign(10);
+
+// lesson 111 //
+function countdown(n) {
+    if (n < 1) {
+        return []
+    } else {
+        const arr = countdown(n - 1)
+        arr.unshift(n)
+        return arr
+    }
+}
+
+// lesson 112 //
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum < startNum) {
+        return []
+    } else {
+        const arr = rangeOfNumbers(startNum, endNum - 1)
+        arr.push(endNum)
+        return arr
+    }
+};
+
+// lesson 113 //
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+        let i = 'block scope';
+        console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+}
