7eeb1d4be64131eb9ad8c8cfe560565fda5ed6a7
Mikhail326 <misha.stelmakh1995@gmail.com>
2022-09-13 17:58:21 +0300

    lessons up to 16 ES6

--- main.js
+++ main.js
@@ -1020,3 +1020,134 @@ function checkScope() {
     console.log('Function scope i is: ', i);
     return i;
   }
+
+  // lesson 2
+const s = [5, 7, 2];
+function editInPlace() {
+    s[0] = 2
+    s[1] = 5
+    s[2] = 7
+}
+editInPlace();
+
+// lesson 3
+function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+    Object.freeze(MATH_CONSTANTS)
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch (ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+// lesson 4
+const magic = () => {
+    return new Date();
+};
+
+// lesson 5
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+// lesson 6
+const increment = (number, value = 1) => number + value;
+
+// lesson 7
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+}
+
+// lesson 8
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+arr2 = [...arr1];  // Change this line
+console.log(arr2);
+
+// lesson 9
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// lesson 10
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
+// lesson 11
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
+
+// lesson 12
+let a = 8, b = 6;
+[a, b] = [b, a]
+
+// lesson 13
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+    const [a, b, ...arr] = list;
+    return arr;
+}
+const arr = removeFirstTwo(source);
+
+// lesson 14
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+const half = ({ max, min }) => (max + min) / 2.0;
+
+// lesson 15
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+    const failureItems = [];
+    for (let i = 0; i < arr.length; i++) {
+        failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+    }
+    return failureItems;
+}
+const failuresList = makeList(result.failure);
+
+// lesson 16
+const createPerson = (name, age, gender) => {
+    return {
+      name,
+      age,
+    gender
+    };
+  };
+
+// lesson 17
+const bicycle = {
+    gear: 2,
+    setGear (newGear) {
+      this.gear = newGear;
+    }
+  };
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
