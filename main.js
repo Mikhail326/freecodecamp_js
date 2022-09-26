44b2375292498c839ff55f5ac8b146a1840b6099
Mikhail326 <misha.stelmakh1995@gmail.com>
2022-09-26 11:37:44 +0300

    completed Basic data structurs course

--- main.js
+++ main.js
@@ -1386,3 +1386,265 @@ function getNine() {
 }
 let result = getNine();
 console.log(result);
+
+//   -------------------Basic Data Structures-----------------
+
+// lesson 1
+let yourArray = [1, 'hello', true, 2, 3];
+
+// lesson 2
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = 'x';
+console.log(myArray);
+
+// lesson 3
+function mixedNumbers(arr) {
+    arr.push(7, 'VIII', 9)
+    arr.unshift('I', 2, 'three')
+    return arr;
+}
+console.log(mixedNumbers(['IV', 5, 'six']));
+
+// lesson 4
+function popShift(arr) {
+    let popped = arr.pop();
+    let shifted = arr.shift();
+    return [shifted, popped];
+}
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+// lesson 5
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(0, 2)
+arr.splice(1, 2)
+arr.splice(2, 2)
+console.log(arr);
+
+// lesson 6
+function htmlColorNames(arr) {
+    arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond')
+    return arr;
+}
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+// lesson 7
+function forecast(arr) {
+    const result = arr.slice(2, 4)
+    return result;
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+// lesson 8
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        newArr.push([...arr])
+        num--;
+    }
+    return newArr;
+}
+console.log(copyMachine([true, false, true], 2));
+
+// lesson 9
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning', ...fragment, 'is', 'fun']// Change this line
+    return sentence;
+}
+
+console.log(spreadOut());
+
+// lesson 10
+function quickCheck(arr, elem) {
+    return arr.indexOf(elem) !== -1
+}
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+// lesson 11
+function filteredArray(arr, elem) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i].indexOf(elem) == -1) {
+                newArr.push(arr[i])
+                break;
+            }
+        }
+    }
+    return newArr;
+}
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+// lesson 12
+let myNestedArray = [
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+    ['loop', 'shift', 6, 7, 1000, 'method'],
+    ['concat', false, true, 'spread', 'array'],
+    ['mutate', 1327.98, 'splice', 'slice', 'push'],
+    ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth'],
+    [
+        ['deep', 2, false]
+    ],
+    [
+        [
+            ['deeper', 3, true]
+        ]
+    ],
+    [
+        [
+            [
+                [
+                    'deepest', 5, false
+                ]
+            ]
+        ]
+    ]
+];
+
+// lesson 13
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+};
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+console.log(foods);
+
+// lesson 14
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+        totalUsers: 51,
+        online: 42
+    }
+};
+userActivity.data.online = 45;
+console.log(userActivity);
+
+// lesson 15
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+function checkInventory(scannedItem) {
+    return foods[scannedItem]
+}
+console.log(checkInventory("apples"));
+
+// lesson 16
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+console.log(foods);
+
+// lesson 17
+let users = {
+    Alan: {
+        age: 27,
+        online: true
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: true
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+function isEveryoneHere(userObj) {
+    return userObj.hasOwnProperty('Alan') && userObj.hasOwnProperty('Jeff') && userObj.hasOwnProperty('Sarah') && userObj.hasOwnProperty('Ryan')
+}
+console.log(isEveryoneHere(users));
+
+// lesson 18
+const users = {
+    Alan: {
+        online: false
+    },
+    Jeff: {
+        online: true
+    },
+    Sarah: {
+        online: false
+    }
+}
+function countOnline(usersObj) {
+    const result = []
+    for (let user in usersObj) {
+        if (usersObj[user].online === true) {
+            result.push(true)
+        }
+    }
+    return result.length
+}
+console.log(countOnline(users));
+
+// lesson 19
+let users = {
+    Alan: {
+        age: 27,
+        online: false
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: false
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+function getArrayOfUsers(obj) {
+    return Object.keys(obj)
+}
+console.log(getArrayOfUsers(users));
+
+// lesson 20
+let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+        username: 'kennethCodesAllDay',
+        joinDate: 'March 26, 2016',
+        organization: 'freeCodeCamp',
+        friends: [
+            'Sam',
+            'Kira',
+            'Tomo'
+        ],
+        location: {
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'USA'
+        }
+    }
+};
+function addFriend(userObj, friend) {
+    userObj.data.friends.push(friend)
+    return userObj.data.friends
+}
+console.log(addFriend(user, 'Pete'));
